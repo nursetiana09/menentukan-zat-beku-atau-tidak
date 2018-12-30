88038a7baf6101773628294673da090270d13bf5
Nursetiana Situmorang <45727455+nursetiana09@users.noreply.github.com>
2018-12-30 10:55:51 +0700

    Add files via upload

---
+++ mencari bentuk zat beku atau tidak.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "mencari bentuk zat beku atau tidak" << endl;
+    int a;
+    cout<< "masukan drajat benda = "; cin>>a;
+ if (a<0)
+     cout<<"suhu = "<<a<<" (maka zat beku)"<<endl;
+     else if (a>100)
+     cout<<"suhu = "<<a<<" (maka zat gas)"<<endl;
+     else
+     cout<<"suhu = "<<a<<" (maka zat cair)"<<endl;
+     getch();
+}
